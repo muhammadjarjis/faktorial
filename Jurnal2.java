f91ed6137cd217e8d2f5abe1252c9855bb25a816
muhammadjarjis <muhammadjarjis15@gmail.com>
2017-04-08 12:15:57 +0700

    Update Jurnal2.java

--- Jurnal2.java
+++ Jurnal2.java
@@ -25,7 +25,7 @@ public class Jurnal2 {
         for(int x=n; x>=1; x--)
         {
             for(int y=x-1; y>=1; y--){
-                System.out.print(" ");
+                System.out.print("0");
             }
             for(int z=1; z<=((n+1)-x)*2; z++){
                 System.out.print("*");
@@ -36,10 +36,14 @@ public class Jurnal2 {
         for(int i=n; i>=1; i--)
         {
             for(int k=1; k<=n-i; k++){
-                System.out.print(" ");
+                System.out.print("0");
             }
             for(int j=i*2; j>=1; j--){
                 System.out.print("*");
+            }
+            
+            for(int k=1; k<=n-i; k++){
+                System.out.print("0");
             }
             System.out.println();
         }
